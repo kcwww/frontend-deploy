c60901fe8ec1a484dd12f18befe7ea0be5c5c878
kcwww <cwkim0321@gmail.com>
2023-09-09 22:53:05 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { ip } from './hide.js'
 
 function App() {
+
+  const test = async () => {
+    try {
+      const res = await axios.get(ip.ip);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const test2 = async () => {
+    try {
+      const res = await axios.post(ip.ip, {
+        "name": "react",},
+        {
+          headers: {
+          'Content-Type': 'application/json',
+          }
+        });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +36,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={test}>GET Test</button>
+        <button onClick={test2}>POST Test2</button>
         <a
           className="App-link"
           href="https://reactjs.org"
